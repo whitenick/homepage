4248b6fd0efe1a425de6313f82d80084a2e88933
Nick <nw.white22@gmail.com>
2022-11-24 13:29:28 -0600

    remove image optimization

--- v2/homepage/pages/index.tsx
+++ v2/homepage/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Home from '../components/home';
 
@@ -24,7 +23,7 @@ export default function Main() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
           </span>
         </a>
       </footer>
