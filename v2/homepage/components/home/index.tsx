d031d93f2d5bdbc77d1d569f28d02e4097bb9441
Nick <nw.white22@gmail.com>
2022-11-24 13:13:36 -0600

    updated w/ v2 homepage (nextJS)

---
+++ v2/homepage/components/home/index.tsx
@@ -0,0 +1,17 @@
+import { DesktopLayout } from "../../modules/layout/desktop-layout";
+import { DesktopWrapper } from "../../modules/layout/desktop-wrapper";
+
+const Home : React.FunctionComponent<any> = () => {
+    return (
+        // <DesktopLayout>
+        //     <div className="">
+        //         Hello
+        //     </div>
+        // </DesktopLayout>
+        <div className={"text-xl"}>
+            Jobin
+        </div>
+    )
+}
+
+export default Home;
---
+++ v2/homepage/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css';``
+import './tw.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
