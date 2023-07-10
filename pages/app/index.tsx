2ceb2d1cef9b8a1b24240aed27e4b435433e01cc
whitenick <nw.white22@gmail.com>
2023-07-10 18:59:59 -0500

    update

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react';
-import { FiGithub, FiLinkedin } from 'react-icons/fi';
 import Home from '../../modules/home';
-import { NavBar, NavBarVertical } from '../../components/navbar/nav-bar';
-import TypedDiv from '../../components/typed';
-import { PageWrapperWithHeader, SpacePageWrapper } from '../../components/layout/page-wrapper';
 
 const HomePage: React.FunctionComponent<any> = () => {
     return (
