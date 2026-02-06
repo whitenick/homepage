6ae934fa41ec93a557fb0284d421e679f6285335
Builder Agent <builder@serapiolabs.com>
2026-02-06 17:10:07 +0000

    Add legacy/ folder with old homepage implementation
    Preserves original homepage files as reference:
    - modules/home/index.tsx
    - pages/index.tsx
    
    This allows easy comparison or rollback if needed.

---
+++ legacy/modules/home/index.tsx
@@ -0,0 +1,52 @@
+import { PageWrapper } from "../../components/layout/page-wrapper";
+import TypedDiv from "../../components/typed";
+import { UFONavBar } from "../../components/navbar/nav-bar";
+
+const Home : React.FunctionComponent<any> = () => {
+    return (
+        // <PageWrapperWithHeader title={"Nick White - About"}>
+        //     {/* <Header /> */}
+        //     <div className="p-6">
+        //         <span className="text-3xl">Hello, and welcome.</span>
+        //         <br/>
+        //         <br/> 
+        //         <span className="text-xl !leading-10 font-montserrat">I am a software engineer by trade, but my mind wanders. My main interests revolve around tech, business, space, and the outdoors. This site will serve as my playground. Here you can find my current experiments and random thoughts.</span> 
+        //     </div>
+        // </PageWrapperWithHeader>
+        <PageWrapper>
+            {/* <div className={"grid grid-flow-col auto-cols-auto w-[100%]"}>
+                <div className="text-6xl font-montserrat text-Jet">
+                    Nick White
+                </div>
+                <div className="flex justify-end gap-x-2">
+                    <a href="https://github.com/whitenick" target="_blank" className="grid justify-end p-2" title="Github">
+                        <FiLinkedin href="https://github.com/whitenick"/>
+                    </a>
+                    <a href="https://github.com/whitenick" target="_blank" className="grid justify-end p-2" title="Github">
+                        <FiGithub href="https://github.com/whitenick"/>
+                    </a>
+                </div>
+            </div> */}
+            {/* <NavBar items={[
+                {
+                    name: 'About',
+                    route: '/app'
+                },
+                {
+                    name: 'Projects',
+                    route: '/app/projects'
+                },
+                {
+                    name: 'Posts',
+                    route: '/app/posts'
+                }
+            ]} /> */}
+            <UFONavBar />
+            <div className="flex items-center justify-center px-[20%] py-32">
+                <TypedDiv style={{ alignItems: 'center', justifyContent: 'center', fontFamily: 'monospace' }} strings={["Hey! Nice to see you."]} className="text-6xl font-montserrat text-Jet" />
+            </div>
+        </PageWrapper>
+    )
+}
+
+export default Home;
