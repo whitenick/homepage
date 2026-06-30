d6e256843468e288ac43db466ba62cf6edd81e3c
YOUR_NAME <YOUR_EMAIL>
2026-06-30 00:49:34 +0000

    update blog desc

--- components/navbar/v2-index-nav.tsx
+++ components/navbar/v2-index-nav.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const links = [
     { name: "About", href: "/about" },
     { name: "Blog", href: "/blog" },
-    { name: "Lab", href: "/app" },
+    { name: "Lab", href: "https://serapiolabs.com" },
 ] as const;
 
 export const V2IndexNav: React.FunctionComponent<{
